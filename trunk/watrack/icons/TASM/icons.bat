a4b557ed78a9bf378afbc220558d78ce40609e50
cheshirabit@gmail.com <cheshirabit@gmail.com@add53e46-2a32-36ee-6256-172e87055e45>
2011-02-16 21:06:13 +0000

    tasm icons resource compile

--- trunk/watrack/icons/TASM/icons.bat
+++ trunk/watrack/icons/TASM/icons.bat
@@ -1,7 +1,8 @@
 @echo off
-brc32 icons.rc
+brcc32 icons.rc -i%1
 tasm32 watrack_buttons.asm
 tlink32 -Tpd watrack_buttons.obj,watrack_buttons.dll,,,,icons.res
 del *.map
 del *.obj
 del *.res
+move watrack_buttons.dll ..\..\..\bin
