9c8948cd0121bb792ef2ce0462b4c0daef3cc69c
cheshirabit <cheshirabit@gmail.com>
2011-04-15 20:07:20 +0000

    copytoclipboard fix freepascal compilation fix

--- actman/make.bat
+++ actman/make.bat
@@ -1,9 +1,9 @@
 @echo off
+..\delphi\brcc32.exe options.rc -fooptions.res
 if /i '%1' == 'fpc' (
   ..\FPC\bin\fpc.exe actman.dpr %2 %3 %4 %5 %6 %7 %8 %9
 ) else if /i '%1' == 'fpc64' (
   ..\FPC\bin64\ppcrossx64.exe actman.dpr %2 %3 %4 %5 %6 %7 %8 %9
 ) else (
-  ..\delphi\brcc32.exe options.rc -fo..\bin\dcu\options.res
   ..\delphi\dcc32 actman.dpr  %2 %3 %4 %5 %6 %7 %8 %9
 )
--- mradio/make.bat
+++ mradio/make.bat
@@ -1,9 +1,9 @@
 @echo off
+..\delphi\brcc32.exe mradio.rc -fomradio.res
 if /i '%1' == 'fpc' (
   ..\FPC\bin\fpc.exe mradio.dpr %2 %3 %4 %5 %6 %7 %8 %9
 ) else if /i '%1' == 'fpc64' (
   ..\FPC\bin64\ppcrossx64.exe mradio.dpr %2 %3 %4 %5 %6 %7 %8 %9
 ) else (
-  ..\delphi\brcc32.exe mradio.rc -fo..\bin\dcu\mradio.res
   ..\delphi\dcc32 mradio.dpr %2 %3 %4 %5 %6 %7 %8 %9
 )
