1b9c82ee8cce90a5e198f6fc65d7f9bf7f4ce87f
cheshirabit <cheshirabit@gmail.com>
2011-04-16 16:56:48 +0000

    watrack resource compilation fix

--- watrack/make.bat
+++ watrack/make.bat
@@ -1,6 +1,8 @@
 @echo off
 ..\delphi\brcc32.exe res\watrack.rc         -fores\watrack.res
 ..\delphi\brcc32.exe lastfm\lastfm.rc       -folastfm\lastfm.res
+..\delphi\brcc32.exe players\mradio.rc      -foplayers\mradio.res
+..\delphi\brcc32.exe kolframe\frm.rc        -fokolframe\frm.res
 ..\delphi\brcc32.exe popup\popup.rc         -fopopup\popup.res
 ..\delphi\brcc32.exe proto\proto.rc         -foproto\proto.res
 ..\delphi\brcc32.exe stat\stat.rc           -fostat\stat.res
