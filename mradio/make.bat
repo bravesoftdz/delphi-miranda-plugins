d858daf3f975db5e8c53634c52e471d3d8014991
cheshirabit <cheshirabit@gmail.com>
2012-07-31 18:30:02 +0000

    KOL with unicode for mRadio

--- mradio/make.bat
+++ mradio/make.bat
@@ -9,9 +9,9 @@ if /i '%1' == 'fpc' (
 ) else if /i '%1' == 'fpc64' (
   ..\FPC\bin64\ppcrossx64.exe %myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
 ) else if /i '%1' == 'xe2' (
-  ..\XE2\BIN\dcc32.exe -b -dKOL_MCK %myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
+  ..\XE2\BIN\dcc32.exe -b -dUNICODE_CTRLS -dKOL_MCK %myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
 ) else if /i '%1' == 'xe64' (
-  ..\XE2\BIN\dcc64.exe -b -dKOL_MCK %myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
+  ..\XE2\BIN\dcc64.exe -b -dUNICODE_CTRLS -dKOL_MCK %myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
 ) else (
-  ..\delphi\dcc32 -b -dKOL_MCK %myopts% %dprname% %1 %2 %3 %4 %5 %6 %7 %8 %9
+  ..\delphi\dcc32 -b -dUNICODE_CTRLS -dKOL_MCK %myopts% %dprname% %1 %2 %3 %4 %5 %6 %7 %8 %9
 )
