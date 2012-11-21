5b34457555bb7c35e41dcda79e9e1c98b13db097
cheshirabit <cheshirabit@add53e46-2a32-36ee-6256-172e87055e45>
2012-11-21 12:18:12 +0000

    compilation fixes

--- watrack/make.bat
+++ watrack/make.bat
@@ -18,7 +18,7 @@ if /i '%1' == 'fpc' (
 ) else if /i '%1' == 'fpc64' (
   ..\FPC\bin64\ppcrossx64.exe %myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
 ) else if /i '%1' == 'xe2' (
-  ..\XE2\BIN\dcc32.exe%myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
+  ..\XE2\BIN\dcc32.exe %myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
 ) else if /i '%1' == 'xe64' (
   ..\XE2\BIN\dcc64.exe %myopts% %dprname% %2 %3 %4 %5 %6 %7 %8 %9
 ) else (
